fa6b984197864bcbd4b0bddd4309773413344068
D C <dc3@mailbox.org>
2022-08-15 22:47:25 -0300

    Adicionada a classe casting. Casting converte explicitamente tipos de dados.

---
+++ src/casting.java
@@ -0,0 +1,21 @@
+// Casting: conversão explícita de tipo de dados
+
+public class casting {
+
+	public static void main(String[] args) {
+		double a = 5.0;
+		int b = (int) a * 2;
+		
+		System.out.println(b);
+
+		int c,d;
+		double resultado;
+		c = 5;
+		d = 2;
+		resultado = (double) c / d;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
