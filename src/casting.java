01a700e0659ddbd68561a62fac3ee8a4a932f59f
D C <dc3@mailbox.org>
2022-08-16 00:08:33 -0300

    Adição de classe scannerString e scannerInt

--- src/casting.java
+++ src/casting.java
@@ -5,17 +5,17 @@ public class casting {
 	public static void main(String[] args) {
 		double a = 5.0;
 		int b = (int) a * 2;
-		
+
 		System.out.println(b);
 
-		int c,d;
+		int c, d;
 		double resultado;
 		c = 5;
 		d = 2;
 		resultado = (double) c / d;
-		
+
 		System.out.println(resultado);
-		
+		System.out.println();
 	}
 
 }
